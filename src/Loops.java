f60ed9938abeb949c378d32c3598143b3ae63470
Mohd Javed <mjaved@webmsi.com>
2022-08-18 15:42:00 +0530

    typecasting

---
+++ src/Loops.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+        // While loop syntax
+        //        for (int i=1; i <= n; i++) {
+        //          body here
+        //        }
+        // shortcut for (for) loop is fori
+//        for (int i=1; i <= n; i++) {
+            //System.out.println(i);
+//            System.out.println(i);
+//        }
+
+        // While loop syntax
+//        int i = 1;
+//        while (i <= n) {
+//            System.out.println(i);
+//            i++;
+//        }
+        int nn = 1;
+        do {
+            System.out.println("Hello World");
+        } while (nn <= 3);
+    }
+}
